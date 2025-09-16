aaf2a8241609f298b039ba6836156dd4c7309800
Henrique <henriqueeliashespanhol@gmail.com>
2025-09-15 21:45:09 -0300

    exercicio

--- index.js
+++ index.js
@@ -16,11 +16,22 @@ const port = 3000
 
 // })
 
-app.get('/api/pessoa/:id', (req, res) => {
+app.get('/exercicio1', (req, res) => {
 
-    const nome = req.query.nome
+    const num1 = req.query.num1
+    const num2 = req.query.num2
+    const soma = (Number(num1) + Number(num2))
+    
+    res.status(200).send(`o resultado da soma de dois numeros inteiros é: ${soma}`)
 
-    res.status(200).send(`hello ${nome}`)
+})
+app.get('/exercicio2', (req, res) => {
+
+    const num1 = req.query.num1
+    const num2 = req.query.num2
+    const soma = (Number(num1) + Number(num2))
+    
+    res.status(200).send(`resultado: ${soma}`)
 
 })
 
